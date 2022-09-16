fa902864d0d5d5ace55cadd3cd240debea9cf6f2
silva94federico <92338429+silva94federico@users.noreply.github.com>
2022-09-16 19:42:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ var prices = [];
 var id = 1;
 $.getJSON("https://script.google.com/macros/s/AKfycbyb1ekFzdhVVCGQCUI49TNXI0e1bWPB1vFsj4cfkyxMmaEZ_gLYgCQat0hmDK7pWyc/exec", function (data) {
 
+
     items.push("<div class=\"tableStyle\">")
     items.push("<table id=\"pricesTable\"><tr><th>Seleccionar</th><th>Producto</th><th>Precio</th></tr>");
     $.each(data, function (key, val) {
-        items.push("<tr><td><input type=\"checkbox\" id=\"cbox" + id + "\" value=\"checkbox" + id + "\"></td><td>" + key + "</td><td>" + val + "</td></tr>");
+        items.push("<tr><td><input type=\"checkbox\" id=\"checkbox" + id + "\" value=\"cbox" + id + "\"></td><td>" + key + "</td><td>$" + val + "</td></tr>");
         prices.push(val);
         id++;
     });
@@ -14,11 +15,7 @@ $.getJSON("https://script.google.com/macros/s/AKfycbyb1ekFzdhVVCGQCUI49TNXI0e1bW
     items.push("</table>");
 
     $("<div/>", {
-        "class": "my-new-div",
+        "class": "inner",
         html: items.join("")
-    }).appendTo("body");
+    }).appendTo("p");
 });
-
-function getData() {
-    return [id, prices];
-}
